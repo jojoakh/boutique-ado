cfa3735ea1d683d5c40848044423c45000bf9936
Semi Akhigbe <akhigbejoy5@gmail.com>
2025-01-31 21:02:58 +0000

    Add checkout templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     
+     # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +68,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'boutique_ado.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -80,8 +84,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
